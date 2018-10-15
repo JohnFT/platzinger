a80bdd09d9110fb5a44102e00d8de79c7a98709a
JhonFT <johnafonseca@gmail.com>
2018-10-15 22:56:14 +0200

    'angular6'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAozoAUdrfHt4xFSlkaGkGDURajCXslqyE',
+    authDomain: 'platzinger-20a71.firebaseapp.com',
+    databaseURL: 'https://platzinger-20a71.firebaseio.com',
+    projectId: 'platzinger-20a71',
+    storageBucket: 'platzinger-20a71.appspot.com',
+    messagingSenderId: '928570443502'
+  }
 };
